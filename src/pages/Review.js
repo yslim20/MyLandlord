25bb6fd468dc28631bbe3777d26131986803e070
yslim20 <ylim27@my.bcit.ca>
2021-11-16 19:46:44 -0800

    finished review lists on review page

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -6,7 +6,7 @@ import Navi from '../comps/Navi';
 import Header from '../comps/Header';
 import Footer from '../comps/Footer';
 import SubHead from '../comps/SubHead';
-import Para from '../comps/Para';
+import RatingStar from '../comps/RatingStar';
 
 
 // ============ Function ============== //
@@ -52,15 +52,59 @@ export default function Review() {
 
 {/* Reviews */}
       <div className = "section_cont">
-        <ul>
+        <ul className ="review_list">
           <li>
             <h2>Housing maintenance</h2>
             <h3>How was the condition of the place?</h3>
-
+            <RatingStar />
+          </li>
+          <li>
+            <h2>Communication</h2>
+            <h3>Was the landlord privode enough communication?</h3>
+            <RatingStar />
+          </li>
+          <li>
+            <h2>Cleanliness</h2>
+            <h3>Was the place clean?</h3>
+            <RatingStar />
+          </li>
+          <li>
+            <h2>Availability</h2>
+            <h3>Was the length of renting period reasonable?</h3>
+            <RatingStar />
+          </li>
+          <li>
+            <h2>Accracy</h2>
+            <h3>Was the place identical as shown on picture?</h3>
+            <RatingStar />
+          </li>
+          <li>
+            <h2>Would you recommend this landlord to others?</h2>
+            <div className = "thumb_con flex-row">
+              <a href="" target="" className="flex-row thumb thumbUp">
+               <img src = "/icons/icon_thumbup.svg" alt = "yes" />
+               <p>Yes</p>
+              </a>
+              <a href="" target="" className="flex-row thumb thumbDown">
+               <img src = "/icons/icon_thumbdown.svg" alt = "no" />
+               <p>No</p>
+              </a>              
+            </div>
           </li>
         </ul>
-
       </div> {/* review ends */}
+
+      <div className = "section_cont">
+        <h2>Add an image</h2>
+        <h3>Upload any images to support your experience!</h3>
+        <ul>
+          <li>
+            <a href="" target="" className = "addImg">
+              <img src = "/icons/icon_addimg.png" alt = "no" />
+            </a>
+          </li>
+        </ul>
+      </div>
       
 
 {/* // ============ Bottom Navigation */}
